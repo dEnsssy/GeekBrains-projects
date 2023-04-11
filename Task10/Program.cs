ab180424ddce2b66b8dbcc95c6d88b22feee7398
dEnsssy <kravchuk.n.o@gmail.com>
2023-04-11 17:29:34 +0300

    do Task10 + do Task13 + do task15

---
+++ Task10/Program.cs
@@ -0,0 +1,20 @@
+﻿// Задача 10: Напишите программу, которая принимает на вход трёхзначное число и на выходе показывает вторую цифру этого числа.
+
+// 456 -> 5
+// 782 -> 8
+// 918 -> 1
+
+Console.Write("Введите число: ");
+int number = Convert.ToInt32(Console.ReadLine());
+if(number>99 && number<1000)
+{
+   int result = secondDigit(number);
+   Console.WriteLine(result);
+}
+else
+   Console.WriteLine("Вы ввели не трёхзначное число");
+
+int secondDigit(int num)
+{
+   return num % 100 / 10;
+}
---
+++ Task15/Program.cs
@@ -0,0 +1,30 @@
+﻿// Задача 15: Напишите программу, которая принимает на вход цифру, обозначающую день недели, и проверяет, является ли этот день выходным.
+
+// 6 -> да
+// 7 -> да
+// 1 -> нет
+bool WeekDays(int num)
+{
+   if(num > 0 && num < 8)
+      return true;
+   else
+      return false;
+}
+bool DayOff(int num1)
+{
+   if (num1>5)
+      return true;
+   else
+      return false;
+}
+
+Console.Write("Введите число: ");
+int number = Convert.ToInt32(Console.ReadLine());
+
+if (WeekDays(number))
+   if (DayOff(number))
+      Console.WriteLine("Да");
+   else
+      Console.WriteLine("Нет");
+else
+   Console.WriteLine("Такого дня недели нет.");
