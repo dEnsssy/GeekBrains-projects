5b265566f84580c1fda3a9bef3e7d371b5a1798a
dEnsssy <kravchuk.n.o@gmail.com>
2023-04-18 15:31:41 +0300

    Task24+26+28+30

---
+++ Task28/Program.cs
@@ -0,0 +1,22 @@
+﻿// Задача 28: Напишите программу, которая
+// принимает на вход число N и выдаёт
+// произведение чисел от 1 до N.
+// 4 -> 24
+// 5 -> 120
+
+int PowNumbers(int num)
+{
+   int pow = 1;
+   for (int i = 1; i <= num; i++)
+   {
+      checked
+      {
+         pow *= i;
+      }
+   }
+   return pow;
+}
+Console.WriteLine("Введите целое положительное число: ");
+int number =Convert.ToInt32(Console.ReadLine());
+int result = PowNumbers(number);
+Console.WriteLine(result);
